eee5b3c5bb894fcde54d2af7a0c3609077fedd31
unknown <cfraleigh@matrixlt0474.office.mymatrixx.com>
2015-06-08 16:37:30 -0400

    created repo

---
+++ Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz4
+{
+    public class FizzBuzz
+    {
+        public String doFizzBuzz(int upperBound)
+        {
+            var value = "";
+
+            for(int i=0; i <= upperBound; i++)
+            {
+                if (i %5==0 && i%3==0 && i != 0)
+                {
+                    value += "FizzBuzz";
+                }
+                else if(i%5==0 && i != 0)
+                {
+                    value += "Buzz";
+                }
+                else if (i%3==0 && i != 0)
+                {
+                    value += "Fizz";
+                }
+                else
+                {
+                    value += i.ToString();
+                }
+            }
+            
+            return value;
+
+
+
+        }
+    }
+}
---
+++ FizzBuzzTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace FizzBuzz4
+{
+    class FizzBuzzTest
+    {
+        [Test]
+        public void returnZeroWhenPazzingZeroTest()
+        {
+            var zeroTest = new FizzBuzz();
+
+            var testValue = zeroTest.doFizzBuzz(0);
+
+            Assert.That(testValue, Is.EqualTo("0"));
+
+
+        }
+        [Test]
+        public void confirmMultipleofThreeReturnsFizzTest()
+        {
+            var fizzTest = new FizzBuzz();
+
+            var testValue = fizzTest.doFizzBuzz(6);
+
+            Assert.That(testValue, Is.EqualTo("Fizz"));
+        }
+        [Test]
+        public void confirmMultipleOfFiveReturnsBuzzTest()
+        {
+            var fizzTest = new FizzBuzz();
+
+            var testValue = fizzTest.doFizzBuzz(5);
+
+            Assert.That(testValue, Is.EqualTo("Buzz"));
+        }
+        [Test]
+        public void confirmLoopRetunsFizzBuzzStringTest()
+        {
+            var fizzTest = new FizzBuzz();
+
+            var testValue = fizzTest.doFizzBuzz(15);
+
+            Assert.That(testValue, Is.EqualTo("012Fizz4BuzzFizz78FizzBuzz11Fizz1314FizzBuzz"));
+        }
+    }
+}
